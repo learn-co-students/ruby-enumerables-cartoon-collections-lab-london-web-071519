df9da99649bcf93317052753b7da5a5a50006b0b
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-06-29 10:56:17 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,15 +3,15 @@ dwarves.each_with_index{|number, i=1| puts "/#{i+1}.*#{number}/ "}
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |x| x.capitalize + "!"}
+  planeteer_calls.map { |i| i.capitalize + "!"}
 end
 
 def long_planeteer_calls(calls)
-calls.any? {|x| x.length > 4}
+calls.any? {|i| i.length > 4}
 end
 
 def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-cheese.find {|type| cheese_types.include?(type)}
+cheese.find {|i| cheese_types.include?(i)}
 end
